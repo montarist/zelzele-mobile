958992e0748ff38f453f48d674ac70cfd28a47f4
Hasan Eren Keskin <erenkeskin95@gmail.com>
2023-03-27 03:40:20 +0300

    refactor: Update icons, prepare Android release and ios build problem fix

--- ios/ZelzeleSon100Deprem/AppDelegate.mm
+++ ios/ZelzeleSon100Deprem/AppDelegate.mm
@@ -6,7 +6,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-  self.moduleName = @"ZelzeleSon100Deprem"|@"ZelzeleSon100Deprem";
+  self.moduleName = @"ZelzeleSon100Deprem";
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
